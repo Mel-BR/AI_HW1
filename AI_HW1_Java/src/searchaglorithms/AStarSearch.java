f41e819b2a0226949eff4e7befc39d0b0ca5f6fe
Mel-BR <melissa.benali-richard@etu.utc.fr>
2015-09-19 13:35:40 -0500

    Creation of the AStarSearch

---
+++ AI_HW1_Java/src/searchaglorithms/AStarSearch.java
@@ -0,0 +1,173 @@
+package searchaglorithms;
+
+import entities.Node;
+import entities.Parser;
+import static entities.Parser.FIN;
+import static entities.Parser.START;
+import static java.lang.Math.abs;
+import java.util.PriorityQueue;
+
+
+public class AStarSearch {
+    
+    private int[][] maze;
+    private int[][] solution;
+    private PriorityQueue<Node> pq;
+    private int[][] expandedNodes;
+    private int expandedNodesCounts;
+    int solCost;
+    int xGoal;
+    int yGoal;
+    
+
+    public AStarSearch(int[][] maze) {
+        this.maze = maze;
+        this.solution = null;
+        this.pq = new PriorityQueue<Node>();
+        this.expandedNodesCounts = 0;
+        this.solCost = 0;
+        
+        this.solution = new int[this.maze.length][this.maze[0].length];
+        
+        for(int i = 0; i < maze.length; i++)
+            for (int j = 0; j < maze[i].length; j++)
+                this.solution[i][j] = this.maze[i][j];
+        
+        findStartFin();
+		
+    }
+    
+    
+    /**
+     * Find the starting position and add the starting node to priority queue.
+     * Find the location of the goal
+     */    
+    private void findStartFin() {
+        this.pq.clear();
+        this.expandedNodes = new int[maze.length][maze[0].length];
+
+        //findind start and goal node
+        for(int i = 0 ; i < this.maze.length ; i++)
+        {
+            for(int j = 0 ; j < this.maze[0].length ; j++) 
+            {
+                if (this.maze[i][j]  == START)
+                {
+                    int h = heuristic(i, j);
+                    int cost = 0;
+                    this.pq.add(new Node(null, i, j, cost, h));
+                    this.expandedNodesCounts++;
+                }
+                else if (this.maze[i][j]  == FIN)
+                {
+                    this.xGoal = i;
+                    this.yGoal = j;
+                }
+            }
+        }
+    }
+    
+    /**
+     * search algo for A* Search 
+     */
+    public void Search(){
+        
+        while (pq.size() > 0) {
+            Node currentNode = pq.remove();
+            int x = currentNode.getX();
+            int y = currentNode.getY();
+            if (this.solution[currentNode.getX()][currentNode.getY()] == FIN) {
+                markSolution(currentNode);
+                break;
+            }
+            else{
+                //Add all adjacent nodes that are not WALLS to the list or update the existing ones
+                //Left
+                checkAndAddNodeToList(currentNode, x-1,y);
+                //Up
+                checkAndAddNodeToList(currentNode, x,y-1);
+                //Right
+                checkAndAddNodeToList(currentNode, x+1,y);
+                //Down
+                checkAndAddNodeToList(currentNode, x,y+1);
+            }
+            
+        }
+        System.out.println("Failure");
+    }
+
+     /**
+     * update the solution matrix to mark the found path
+     * @param Node
+     */
+    public void markSolution(Node currentNode){
+        while (currentNode!=null){
+            this.solution[currentNode.getX()][currentNode.getY()]=START;
+            this.solCost++;
+            currentNode = currentNode.getParent();
+        }
+    }
+    
+    /**
+     * print the maze with solution path, the cost, and the number of nodes expanded 
+     */
+    public void printSolution() {
+        System.out.println("Solution:");
+        Parser.displayBeautifulMatrix(solution);
+        System.out.println("\n Cost : "+this.solCost);       
+        System.out.println("\n Nodes expanded : "+ this.expandedNodesCounts);
+    }
+       
+     /**
+     * Add node to list if not already visited, update them if necessary and if already visited
+     * @param currentNode
+     * @param x
+     * @param y
+     */
+    private void checkAndAddNodeToList(Node currNode, int x, int y)
+    {
+        int cost = currNode.getCurrentPathCost()+1;
+        int h = heuristic(x,y);
+        if (h+cost < this.maze[x][y]) {
+                Node n = existNode(x,y);
+                //node already expanded, need to change totalCost value
+                if (n!=null){ 
+                    n.setCurrentTotalCost(h+cost);
+                }
+                else{ // node not already expanded
+                    this.expandedNodesCounts++;
+                }
+                this.pq.add(new Node(currNode, x, y, cost, h));
+
+                this.maze[x][y] = h+cost;
+        }
+  
+    }
+
+    /**
+     * find the heuristic value for a given node
+     * @param x
+     * @param y
+     * @return manhattan distance between the node position x,y and the goal position
+     */
+    private int heuristic(int x, int y){
+        return abs(this.xGoal-x)+abs(this.yGoal-y);
+    }
+
+    
+    
+     /**
+     * check if a node is already in the priority queue
+     * @param x
+     * @param y
+     * @return true if already exists, false otherwise
+     */
+    private Node existNode(int x, int y){
+        for (Node node : this.pq) {
+            if (x == node.getX() && y == node.getY() ){
+                return node;
+            }
+        }
+        return null;
+    }
+}
