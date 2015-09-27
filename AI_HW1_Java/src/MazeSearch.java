9baaeb54a5ceca78c9dfe7209f41b9046b538ab9
Mel-BR <melissa.benali-richard@etu.utc.fr>
2015-09-27 16:42:14 -0500

    clean main method

--- AI_HW1_Java/src/MazeSearch.java
+++ AI_HW1_Java/src/MazeSearch.java
@@ -5,10 +5,10 @@ import searchaglorithms.PacMan;
 import entities.Parser;
 import searchaglorithms.AStarSearch;
 import searchaglorithms.BFSSearch;
-import searchaglorithms.AStarSearchPenalizingTurns;
 import searchaglorithms.DFSSearch;
 import searchaglorithms.GreedySearch2;
 import entities.Parser;
+import searchaglorithms.AStarSearchPenalizingTurnsMD;
 
 
 
@@ -17,21 +17,27 @@ public class MazeSearch {
 	public static void main(String[] args) {
             
         /* Parsing input */
-		
-		//Classic search		
-		ArrayList<ArrayList<Integer>> maze = Parser.parse("src/input/openMaze.txt");
-                int[][] matrix = Parser.getMatrix(maze);
+
+        /* Available files*/
+        ArrayList<ArrayList<Integer>> maze = Parser.parse("src/input/mediumMaze.txt");		
+        //ArrayList<ArrayList<Integer>> maze = Parser.parse("src/input/bigMaze.txt");		
+        //ArrayList<ArrayList<Integer>> maze = Parser.parse("src/input/openMaze.txt");		
+        //ArrayList<ArrayList<Integer>> maze = Parser.parse("src/input/smallTurns.txt");		
+        //ArrayList<ArrayList<Integer>> maze = Parser.parse("src/input/bigTurns.txt");
+            
+        
+        //Classic search
+        int[][] matrix = Parser.getMatrix(maze);
                 
         //Animated search
-		ArrayList<ArrayList<Integer>> pacMaze = Parser.parse("src/input/complicatedGhostPathMaze.txt"); 
-                int[][] pacMat = Parser.getMatrix(pacMaze);
+        ArrayList<ArrayList<Integer>> pacMaze = Parser.parse("src/input/complicatedGhostPathMaze.txt"); 
+        int[][] pacMat = Parser.getMatrix(pacMaze);
 
                 
         // PART 1.1
         /* Search Algorithms */
         
         //DFSSearch
-
         /*DFSSearch dfsSearch = new DFSSearch(matrix);
         dfsSearch.Search();
         dfsSearch.printSolution();*/
@@ -40,30 +46,32 @@ public class MazeSearch {
         //BFSSearch
         //BFSSearch bfsSearch = new BFSSearch(matrix);
         //Search and displays the solution
-        //bfsSearch.search();           
+        //bfsSearch.search();             
 
-        
         
         //Greedy Search
         /*BFSSearch2 greedySearch = new AStarSearch2(matrix);
         greedySearch.search();
         greedySearch.printSolution();*/
-      
-        BFSSearch2 pacman = new BFSSearch2(matrix);
-        pacman.search();
-        pacman.printSolution();
+
         
         //A* Search
         /*AStarSearch aStarSearch = new AStarSearch(matrix);
         aStarSearch.Search();
         aStarSearch.printSolution();*/
-
-        
+             
+                
         //PART 1.2
-        //A* Search 
-        /*AStarSearchPenalizingTurns aStarPenalizingTurns = new AStarSearchPenalizingTurns(matrix);
-        aStarPenalizingTurns.Search();
-        aStarPenalizingTurns.printSolution();*/
+        //A* Search penalizing turns
+        /*int forwardCost = 2;
+        int turnCost = 1;
+        String typeHeuristic = "New"; 
+        // can be either "MD" or "New"
+        AStarSearchPenalizingTurnsMD aStarPenalizingTurnsMD = new AStarSearchPenalizingTurnsMD(matrix, turnCost,forwardCost,typeHeuristic);
+        aStarPenalizingTurnsMD.Search();
+        aStarPenalizingTurnsMD.printSolution();*/
+        
+        
 
 	}
 }
